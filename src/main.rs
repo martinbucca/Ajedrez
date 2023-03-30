38d57d2eb7bab84379121bb24e3527a8487f42f6
Martin Bucca <martinbucca02@gmail.com>
2023-03-30 20:07:15 -0300

    programa andando -> codigo a corregir

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,302 @@ use std::io::Error;
 use std::io::{BufRead, BufReader};
 use std::process::exit;
 
-
-struct Position{
+#[derive(Debug, PartialEq)]
+struct Position {
     row: usize,
     column: usize,
 }
-struct Piece{
-    position: Position,
-    typ: char,
-    color: String,
-    posible_moves: Vec<Position>,
+
+trait PosiblesMovientos {
+    fn posibles_movimientos(&self, position: &Position) -> Vec<Position>;
+}
+#[derive(Debug)]
+struct Alfil;
+impl PosiblesMovientos for Alfil {
+    fn posibles_movimientos(&self, position: &Position) -> Vec<Position> {
+        let mut movimientos: Vec<Position> = vec![];
+        let mut i = position.row;
+        let mut j = position.column;
+        while i > 0 && j > 0{
+            i -= 1;
+            j -= 1;
+        }
+        while i <= 8 && j <= 8{
+            movimientos.push(Position { row: i, column: j});
+            i += 1;
+            j += 1;
+        }
+        i = position.row;
+        j = position.column;
+        while i > 0 && j < 8{
+            i -= 1;
+            j += 1;
+        } 
+        while i < 8 && j > 0{
+            movimientos.push(Position{row: i, column: j});
+            i += 1;
+            j -= 1;
+
+        }
+        movimientos
+    }
+}
+#[derive(Debug)]
+struct Rey;
+impl PosiblesMovientos for Rey {
+    fn posibles_movimientos(&self, position: &Position) -> Vec<Position> {
+        let movimientos: Vec<Position> = vec![
+            Position {
+                row: position.row + 1,
+                column: position.column - 1,
+            },
+            Position {
+                row: position.row + 1,
+                column: position.column + 1,
+            },
+            Position {
+                row: position.row - 1,
+                column: position.column - 1,
+            },
+            Position {
+                row: position.row - 1,
+                column: position.column + 1,
+            },
+            Position {
+                row: position.row,
+                column: position.column - 1,
+            },
+            Position {
+                row: position.row,
+                column: position.column + 1,
+            },
+            Position {
+                row: position.row + 1,
+                column: position.column,
+            },
+            Position {
+                row: position.row - 1,
+                column: position.column,
+            },
+        ];
+        movimientos
+    }
+}
+#[derive(Debug)]
+struct Dama;
+impl PosiblesMovientos for Dama {
+    fn posibles_movimientos(&self, position: &Position) -> Vec<Position> {
+        let mut movimientos: Vec<Position> = vec![];
+        for i in 1..=8 {
+            movimientos.push(Position {
+                row: position.row,
+                column: i,
+            });
+            movimientos.push(Position {
+                row: i,
+                column: position.column,
+            })
+        }
+        let mut i = position.row;
+        let mut j = position.column;
+        while i > 0 && j > 0{
+            i -= 1;
+            j -= 1;
+        }
+        while i <= 8 && j <= 8{
+            movimientos.push(Position { row: i, column: j});
+            i += 1;
+            j += 1;
+        }
+        i = position.row;
+        j = position.column;
+        while i > 0 && j < 8{
+            i -= 1;
+            j += 1;
+        } 
+        while i < 8 && j > 0{
+            movimientos.push(Position{row: i, column: j});
+            i += 1;
+            j -= 1;
+
+        }
+        movimientos
+    }
+}
+#[derive(Debug)]
+struct Torre;
+impl PosiblesMovientos for Torre {
+    fn posibles_movimientos(&self, position: &Position) -> Vec<Position> {
+        let mut movimientos: Vec<Position> = vec![];
+        for i in 1..=8 {
+            movimientos.push(Position {
+                row: position.row,
+                column: i,
+            });
+            movimientos.push(Position {
+                row: i,
+                column: position.column,
+            })
+        }
+        movimientos
+    }
+}
+#[derive(Debug)]
+struct Caballo;
+impl PosiblesMovientos for Caballo {
+    fn posibles_movimientos(&self, position: &Position) -> Vec<Position> {
+        let movimientos: Vec<Position> = vec![
+            Position {
+                row: (position.row as isize - 1) as usize,
+                column: (position.column as isize + 2) as usize,
+            },
+            Position {
+                row: (position.row as isize - 1) as usize,
+                column: (position.column as isize - 2) as usize,
+            },
+            Position {
+                row: (position.row as isize + 1) as usize,
+                column: (position.column as isize + 2) as usize,
+            },
+            Position {
+                row: (position.row as isize + 1) as usize,
+                column: (position.column as isize - 2) as usize,
+            },
+            Position {
+                row: (position.row as isize - 2) as usize,
+                column: (position.column as isize + 1) as usize,
+            },
+            Position {
+                row: (position.row as isize - 2) as usize,
+                column: (position.column as isize - 1) as usize,
+            },
+            Position {
+                row: (position.row as isize + 2) as usize,
+                column: (position.column as isize + 1) as usize,
+            },
+            Position {
+                row: (position.row as isize + 2) as usize,
+                column: (position.column as isize - 1) as usize,
+            },
+        ];
+        movimientos
+        // logica para obtener los posibles movimientos del alfil
+    }
+}
+#[derive(Debug)]
+struct PeonBlanco;
+impl PosiblesMovientos for PeonBlanco {
+    fn posibles_movimientos(&self, position: &Position) -> Vec<Position> {
+        let movimientos: Vec<Position> = vec![
+            Position {
+                row: position.row - 1,
+                column: position.column + 1,
+            },
+            Position {
+                row: position.row - 1,
+                column: position.column - 1,
+            },
+        ];
+        movimientos
+    }
+}
+#[derive(Debug)]
+struct PeonNegro;
+impl PosiblesMovientos for PeonNegro {
+    fn posibles_movimientos(&self, position: &Position) -> Vec<Position> {
+        let movimientos: Vec<Position> = vec![
+            Position {
+                row: position.row + 1,
+                column: position.column - 1,
+            },
+            Position {
+                row: position.row + 1,
+                column: position.column + 1,
+            },
+        ];
+        movimientos
+    }
+}
+#[derive(Debug)]
+enum Piece {
+    Alfil(Alfil),
+    Torre(Torre),
+    Caballo(Caballo),
+    Rey(Rey),
+    Dama(Dama),
+    PeonBlanco(PeonBlanco),
+    PeonNegro(PeonNegro),
 }
 
+#[derive(Debug)]
 struct Chessboard {
     black: Piece,
     white: Piece,
+    white_position: Position,
+    black_position: Position,
 }
+
+fn puede_capturar(piece: &Piece, piece_position: &Position, oponent_position: &Position) -> bool {
+    match piece {
+        Piece::Torre(torre) => {
+            for mov in torre.posibles_movimientos(piece_position) {
+                if mov == *oponent_position {
+                    return true;
+                }
+            }
+            false
+        }
+        Piece::Alfil(alfil) => {
+            for mov in alfil.posibles_movimientos(piece_position) {
+                if mov == *oponent_position {
+                    return true;
+                }
+            }
+            false
+        }
+        Piece::Caballo(caballo) => {
+            for mov in caballo.posibles_movimientos(piece_position) {
+                if mov == *oponent_position {
+                    return true;
+                }
+            }
+            false
+        }
+        Piece::Rey(rey) => {
+            for mov in rey.posibles_movimientos(piece_position) {
+                if mov == *oponent_position {
+                    return true;
+                }
+            }
+            false
+        }
+        Piece::Dama(dama) => {
+            for mov in dama.posibles_movimientos(piece_position) {
+                if mov == *oponent_position {
+                    return true;
+                }
+            }
+            false
+        }
+        Piece::PeonBlanco(peon) => {
+            for mov in peon.posibles_movimientos(piece_position) {
+                if mov == *oponent_position {
+                    return true;
+                }
+            }
+            false
+        }
+        Piece::PeonNegro(peon) => {
+            for mov in peon.posibles_movimientos(piece_position) {
+                if mov == *oponent_position {
+                    return true;
+                }
+            }
+            false
+        }
+    }
+}
+
 fn main() {
     let file_name = match read_arguments(env::args().collect()) {
         None => {
@@ -46,36 +326,102 @@ fn main() {
         println!("{}", error);
         exit(0);
     }
-    create_chessboard(&table);
-    println!("MI TABLERO ES VALIDO!!!\n {:?}", table);
+    let chessboard = create_chessboard(&table);
+    let black_can_capture = puede_capturar(
+        &chessboard.black,
+        &chessboard.black_position,
+        &chessboard.white_position,
+    );
+    let white_can_capture = puede_capturar(
+        &chessboard.white,
+        &chessboard.white_position,
+        &chessboard.black_position,
+    );
+    return_result(black_can_capture, white_can_capture);
+
 }
 
-
-fn create_chessboard(table: &Vec<Vec<char>>) -> Chessboard{
-    let black_piece: Piece;
-    let white_piece: Piece;
-    
-    for (number_of_row, row) in table.into_iter().enumerate(){
-        for (number_of_column, char) in row.into_iter().enumerate(){
-            match char{
+fn return_result(black_can_capture: bool, white_can_capture: bool) {
+    let result = (black_can_capture, white_can_capture);
+    match result {
+        (false, true) => println!("B"),
+        (true, false) => println!("N"),
+        (true, true) => println!("E"),
+        (false, false) => println!("P"),
+    }
+}
+fn create_chessboard(table: &[Vec<char>]) -> Chessboard {
+    let mut black: Piece = Piece::Rey(Rey);
+    let mut white: Piece = Piece::Rey(Rey);
+    let mut black_position = Position { row: 0, column: 0 };
+    let mut white_position = Position { row: 0, column: 0 };
+    for (number_of_row, row) in table.iter().enumerate() {
+        for (number_of_column, char) in row.iter().enumerate() {
+            let position = Position {
+                row: number_of_row + 1,
+                column: number_of_column + 1,
+            };
+            match char {
                 '_' => (),
-                'T' | 'A' | 'R' | 'D' | 'C' | 'P' => {
-                    black_piece = Piece {
-                        position: Position { row: (number_of_row), column: (number_of_column) },
-                        typ: *char,
-                        color: "black".to_string(),
-                        posible_moves: get_posible_moves_of(char, position)
-                    }
-                },
-                't' | 'a' | 'r' | 'd' | 'c' | 'p' => (),
+                'T' => {
+                    black = Piece::Torre(Torre);
+                    black_position = position
+                }
+                'A' => {
+                    black = Piece::Alfil(Alfil);
+                    black_position = position
+                }
+                'R' => {
+                    black = Piece::Rey(Rey);
+                    black_position = position
+                }
+                'D' => {
+                    black = Piece::Dama(Dama);
+                    black_position = position
+                }
+                'C' => {
+                    black = Piece::Caballo(Caballo);
+                    black_position = position
+                }
+                'P' => {
+                    black = Piece::PeonNegro(PeonNegro);
+                    black_position = position
+                }
+                't' => {
+                    white = Piece::Torre(Torre);
+                    white_position = position
+                }
+                'a' => {
+                    white = Piece::Alfil(Alfil);
+                    white_position = position
+                }
+                'r' => {
+                    white = Piece::Rey(Rey);
+                    white_position = position
+                }
+                'd' => {
+                    white = Piece::Dama(Dama);
+                    white_position = position
+                }
+                'c' => {
+                    white = Piece::Caballo(Caballo);
+                    white_position = position
+                }
+                'p' => {
+                    white = Piece::PeonBlanco(PeonBlanco);
+                    white_position = position
+                }
                 _ => (),
             }
         }
     }
-    Chessboard { black: (Piece { position: (    Position { row: (), column: () }), typ: (), color: (), posible_moves: () }), white: () }
-    
+    Chessboard {
+        black,
+        white,
+        black_position,
+        white_position,
+    }
 }
-
 fn validate_table(table: &Vec<Vec<char>>) -> Option<String> {
     if table.len() != 8 {
         return Some(
