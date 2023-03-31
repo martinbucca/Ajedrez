8704eaadc5c73d112d766917e4019ae786b86e71
Martin Bucca <martinbucca02@gmail.com>
2023-03-31 11:42:13 -0300

    mas modificaciones

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,74 @@ use std::io::Error;
 use std::io::{BufRead, BufReader};
 use std::process::exit;
 
+fn get_one_distance_upwards_diagonal_moves(position: &Position) -> Vec<Position> {
+    let mut possible_moves: Vec<Position> = vec![];
+    let (piece_row, piece_column) = (position.row, position.column);
+    if piece_row - 1 > 0 {
+        if piece_column - 1 > 0 {
+            possible_moves.push(Position {
+                row: piece_row - 1,
+                column: piece_column - 1,
+            })
+        }
+        if piece_column + 1 < 9 {
+            possible_moves.push(Position {
+                row: piece_row - 1,
+                column: piece_column + 1,
+            })
+        }
+    }
+    possible_moves
+}
+
+fn get_one_distance_downwards_diagonal_moves(position: &Position) -> Vec<Position> {
+    let mut possible_moves: Vec<Position> = vec![];
+    let (piece_row, piece_column) = (position.row, position.column);
+    if piece_row + 1 < 9 {
+        if piece_column - 1 > 0 {
+            possible_moves.push(Position {
+                row: piece_row + 1,
+                column: piece_column - 1,
+            })
+        }
+        if piece_column + 1 < 9 {
+            possible_moves.push(Position {
+                row: piece_row + 1,
+                column: piece_column + 1,
+            })
+        }
+    }
+    possible_moves
+}
+fn get_one_distance_straight_moves(position: &Position) -> Vec<Position> {
+    let mut possible_moves = vec![];
+    let (piece_row, piece_column) = (position.row, position.column);
+    if piece_row + 1 < 9 {
+        possible_moves.push(Position {
+            row: piece_row + 1,
+            column: piece_column,
+        });
+    }
+    if piece_row - 1 > 0 {
+        possible_moves.push(Position {
+            row: piece_row - 1,
+            column: piece_column,
+        });
+    }
+    if piece_column + 1 < 9 {
+        possible_moves.push(Position {
+            row: piece_row,
+            column: piece_column + 1,
+        });
+    }
+    if piece_column - 1 > 0 {
+        possible_moves.push(Position {
+            row: piece_row,
+            column: piece_column - 1,
+        });
+    }
+    possible_moves
+}
 fn get_straight_moves(position: &Position) -> Vec<Position> {
     let (piece_row, piece_column) = (position.row, position.column);
     let mut possible_moves: Vec<Position> = vec![];
@@ -23,6 +91,99 @@ fn get_straight_moves(position: &Position) -> Vec<Position> {
     }
     possible_moves
 }
+fn get_diagonal_moves(position: &Position) -> Vec<Position> {
+    let mut possible_moves = get_principal_diagonal_moves(position);
+    possible_moves.extend(get_secondary_diagonal_moves(position));
+    possible_moves
+}
+
+fn get_principal_diagonal_moves_with_piece_in_central_diagonal(
+    position: &Position,
+) -> Vec<Position> {
+    let mut possible_moves = vec![];
+    // columna = fila, da lo mismo cual elijo
+    let piece_row_and_column = position.row;
+    for i in 1..=8 {
+        if i != piece_row_and_column {
+            possible_moves.push(Position { row: i, column: i });
+        }
+    }
+    possible_moves
+}
+fn get_principal_diagonal_moves_with_piece_in_superior_diagonal(
+    position: &Position,
+) -> Vec<Position> {
+    let mut possible_moves: Vec<Position> = vec![];
+    let (piece_row, piece_column) = (position.row, position.column);
+    // la fila donde empieza la diagonal va a ser siempre 1
+    let mut i = 1;
+    // la columna de comienzo se puede calcular de esta forma: columna_de_pieza - distancia_entre_fila_1_y_fila_pieza
+    let mut j = piece_column - (piece_row - 1);
+    while j <= 8 {
+        if i != piece_row && j != piece_column {
+            possible_moves.push(Position { row: i, column: j });
+        }
+        i += 1;
+        j += 1;
+    }
+    possible_moves
+}
+
+fn get_principal_diagonal_moves_with_piece_in_inferior_diagonal(
+    position: &Position,
+) -> Vec<Position> {
+    let mut possible_moves: Vec<Position> = vec![];
+    let (piece_row, piece_column) = (position.row, position.column);
+    // la fila donde empieza la diagonal siempre va a ser 1
+    let mut j = 1;
+    // la fila de comienzo se puede calcular de esta forma: fila_de_pieza - distancia_entre_columna_1_y_columna_pieza
+    let mut i = piece_row - (piece_column - 1);
+    while i <= 8 {
+        if i != piece_row && j != piece_column {
+            possible_moves.push(Position { row: i, column: j });
+        }
+        (i, j) = (i + 1, j + 1);
+    }
+    possible_moves
+}
+fn get_principal_diagonal_moves(position: &Position) -> Vec<Position> {
+    let mut possible_moves: Vec<Position> = vec![];
+    let (piece_row, piece_column) = (position.row, position.column);
+    // si la pieza esta sobre la diagonal central
+    if piece_row == piece_column {
+        // las filas y columnas van a ser igales por estar en la diagonal central!
+        possible_moves.extend(get_principal_diagonal_moves_with_piece_in_central_diagonal(
+            position,
+        ));
+    }
+    // si la pieza esta por arriba de la diagonal central
+    if piece_column > piece_row {
+        possible_moves
+            .extend(get_principal_diagonal_moves_with_piece_in_superior_diagonal(position));
+    }
+    // si la pieza esta por debajo de la diagonal central
+    if piece_row > piece_column {
+        possible_moves
+            .extend(get_principal_diagonal_moves_with_piece_in_inferior_diagonal(position));
+    }
+    possible_moves
+}
+fn get_secondary_diagonal_moves(position: &Position) -> Vec<Position> {
+    let mut possible_moves: Vec<Position> = vec![];
+    let mut i = position.row;
+    let mut j = position.column;
+    while i > 0 && j < 8 {
+        i -= 1;
+        j += 1;
+    }
+    while i < 8 && j > 0 {
+        possible_moves.push(Position { row: i, column: j });
+        i += 1;
+        j -= 1;
+    }
+    possible_moves
+}
+
 #[derive(Debug, PartialEq)]
 struct Position {
     row: usize,
@@ -36,111 +197,26 @@ trait PossibleMoves {
 struct Alfil;
 impl PossibleMoves for Alfil {
     fn possible_moves(&self, position: &Position) -> Vec<Position> {
-        let mut movimientos: Vec<Position> = vec![];
-        let mut i = position.row;
-        let mut j = position.column;
-        while i > 0 && j > 0 {
-            i -= 1;
-            j -= 1;
-        }
-        while i <= 8 && j <= 8 {
-            movimientos.push(Position { row: i, column: j });
-            i += 1;
-            j += 1;
-        }
-        i = position.row;
-        j = position.column;
-        while i > 0 && j < 8 {
-            i -= 1;
-            j += 1;
-        }
-        while i < 8 && j > 0 {
-            movimientos.push(Position { row: i, column: j });
-            i += 1;
-            j -= 1;
-        }
-        movimientos
+        get_diagonal_moves(position)
     }
 }
 #[derive(Debug)]
 struct Rey;
 impl PossibleMoves for Rey {
     fn possible_moves(&self, position: &Position) -> Vec<Position> {
-        let movimientos: Vec<Position> = vec![
-            Position {
-                row: position.row + 1,
-                column: position.column - 1,
-            },
-            Position {
-                row: position.row + 1,
-                column: position.column + 1,
-            },
-            Position {
-                row: position.row - 1,
-                column: position.column - 1,
-            },
-            Position {
-                row: position.row - 1,
-                column: position.column + 1,
-            },
-            Position {
-                row: position.row,
-                column: position.column - 1,
-            },
-            Position {
-                row: position.row,
-                column: position.column + 1,
-            },
-            Position {
-                row: position.row + 1,
-                column: position.column,
-            },
-            Position {
-                row: position.row - 1,
-                column: position.column,
-            },
-        ];
-        movimientos
+        let mut possible_moves: Vec<Position> = get_one_distance_downwards_diagonal_moves(position);
+        possible_moves.extend(get_one_distance_upwards_diagonal_moves(position));
+        possible_moves.extend(get_one_distance_straight_moves(position));
+        possible_moves
     }
 }
 #[derive(Debug)]
 struct Dama;
 impl PossibleMoves for Dama {
     fn possible_moves(&self, position: &Position) -> Vec<Position> {
-        let mut movimientos: Vec<Position> = vec![];
-        for i in 1..=8 {
-            movimientos.push(Position {
-                row: position.row,
-                column: i,
-            });
-            movimientos.push(Position {
-                row: i,
-                column: position.column,
-            })
-        }
-        let mut i = position.row;
-        let mut j = position.column;
-        while i > 0 && j > 0 {
-            i -= 1;
-            j -= 1;
-        }
-        while i <= 8 && j <= 8 {
-            movimientos.push(Position { row: i, column: j });
-            i += 1;
-            j += 1;
-        }
-        i = position.row;
-        j = position.column;
-        while i > 0 && j < 8 {
-            i -= 1;
-            j += 1;
-        }
-        while i < 8 && j > 0 {
-            movimientos.push(Position { row: i, column: j });
-            i += 1;
-            j -= 1;
-        }
-        movimientos
+        let mut possible_moves = get_straight_moves(position);
+        possible_moves.extend(get_diagonal_moves(position));
+        possible_moves
     }
 }
 #[derive(Debug)]
@@ -195,48 +271,16 @@ impl PossibleMoves for Caballo {
 struct PeonBlanco;
 impl PossibleMoves for PeonBlanco {
     fn possible_moves(&self, position: &Position) -> Vec<Position> {
-        // el peon blanco solo tiene dos posibles movimientos para capturar
-        let mut possible_moves: Vec<Position> = vec![];
-        let (piece_row, piece_column) = (position.row, position.column);
-        if piece_row - 1 > 0 {
-            if piece_column - 1 > 0 {
-                possible_moves.push(Position {
-                    row: piece_row - 1,
-                    column: piece_column - 1,
-                })
-            }
-            if piece_column + 1 < 9 {
-                possible_moves.push(Position {
-                    row: piece_row - 1,
-                    column: piece_column + 1,
-                })
-            }
-        }
-        possible_moves
+        // el peon blanco solo tiene dos posibles movimientos para capturar y solo hacia arriba
+        get_one_distance_upwards_diagonal_moves(position)
     }
 }
 #[derive(Debug)]
 struct PeonNegro;
 impl PossibleMoves for PeonNegro {
     fn possible_moves(&self, position: &Position) -> Vec<Position> {
-        // el peon negro solo tiene dos posibles movimientos para capturar
-        let mut possible_moves: Vec<Position> = vec![];
-        let (piece_row, piece_column) = (position.row, position.column);
-        if piece_row + 1 < 9 {
-            if piece_column - 1 > 0 {
-                possible_moves.push(Position {
-                    row: piece_row + 1,
-                    column: piece_column - 1,
-                })
-            }
-            if piece_column + 1 < 9 {
-                possible_moves.push(Position {
-                    row: piece_row + 1,
-                    column: piece_column + 1,
-                })
-            }
-        }
-        possible_moves
+        // el peon negro solo tiene dos posibles movimientos para capturar y solo hacia abajo
+        get_one_distance_downwards_diagonal_moves(position)
     }
 }
 #[derive(Debug)]
